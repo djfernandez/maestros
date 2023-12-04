6c7618cd0d422f2a224759211a220ffb373bd018
TPP\dfernandez <dfernandez@tpp.com.pe>
2023-12-04 15:11:40 -0500

    Test

--- src/test/java/pe/com/tpp/maestros/MaestrosApplicationTests.java
+++ src/test/java/pe/com/tpp/maestros/MaestrosApplicationTests.java
@@ -1,12 +1,12 @@
 package pe.com.tpp.maestros;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MaestrosApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
